86acbc2841907cb74d20bbfabbd3c2bab55927b3
pitel86 <pitelsergio@gmail.com>
2022-02-17 19:11:25 +0100

    search put fixed

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -1,6 +1,6 @@
 const UserRoutes = require('express').Router();
 const { isAuth } = require('../../utils/middleware/auth')
-const { postNewUser, loginUser, logOut, getUser, putUser } = require('./user.controller')
+const { postNewUser, loginUser, logOut, getUser, putUser, putUserSearch } = require('./user.controller')
 const upload = require('../../utils/middleware/file')
 
 
@@ -9,5 +9,6 @@ UserRoutes.post('/login', loginUser);
 UserRoutes.post('/logout',[isAuth], logOut);
 UserRoutes.get('/:id', [isAuth], getUser);
 UserRoutes.put('/:id',[isAuth],upload.single('photo'), putUser);
+UserRoutes.put('/addSearch/:id',[isAuth], putUserSearch);
 
 module.exports = UserRoutes;
